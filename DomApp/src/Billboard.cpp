9c9e64dfad05e67ad35627c575b0a7198b9b9db2
Anders Nord <anders-nord@live.se>
2012-11-01 12:06:38 +0100

    Added constructor to Billboard class

--- DomApp/src/Billboard.cpp
+++ DomApp/src/Billboard.cpp
@@ -1,6 +1,33 @@
 #include "Billboard.h"
 
+/**
+*@file Billboard.cpp
+*
+*@brief		
+*
+*@details
+*
+*@param		texturename Unique name of a texture. Ex: "texture.png".
+*@param		position Contains the positions in world coordinates.
+*@param		proportions The proportions of the billboardsize according to the world unit length. 
+*/
+Billboard::Billboard(std::string texturename , glm::vec3 position, glm::vec2 proportionsIn)
+{
+	texture = texturename;
+
+	this->proportions = proportionsIn;
+
+	transform = glm::mat4x4(1);
+	transform = glm::translate(transform, position);
+
+
+};
+
+
 void Billboard::draw() {
 	
 	sgct::MessageHandler::Instance()->print("Billboard draw\n");
+
+
+
 }
