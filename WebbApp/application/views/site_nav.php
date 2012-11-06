4f9b1827cc3db8ad15fedb01ae56b89dc1960b93
Viktor Fröberg <viktorfroberg@gmail.com>
2012-11-06 16:31:45 +0100

    Fixed nav.

--- WebbApp/application/views/site_nav.php
+++ WebbApp/application/views/site_nav.php
@@ -3,8 +3,15 @@
 <div id="container">	
 	<div id="nav">
 		<ul>
+		<!--
 		<li><a href="<?php echo base_url(); ?>index.php/site/skapa">Skapa visning</a></li>
 		<li><a href="<?php echo base_url(); ?>index.php/site/andra">Ändra en visning</a></li>
 		<li><a href="<?php echo base_url(); ?>index.php/site/om">Om</a></li>	
+	-->
+
+		<li><?php echo anchor('index.php/site/skapa', 'Skapa visning');?></li>
+		<li><?php echo anchor('index.php/site/andra', 'Ändra en visning');?></li>
+		<li><?php echo anchor('index.php/site/om', 'Om');?></li>
+
 		</ul> 	
 	</div>
