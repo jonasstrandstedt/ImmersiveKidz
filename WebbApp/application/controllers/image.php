52c38484d68b256b361918c504ac89b1fc0ea57a
GabriellaIvarsson <gabriellaivarsson@gmail.com>
2012-11-08 14:18:13 +0100

    Revert "Added test images and threashold, open, close operation."
    This reverts commit 6f4d0c9e97ab91db5bdf444f6cb0913e96462703.

--- WebbApp/application/controllers/image.php
+++ WebbApp/application/controllers/image.php
@@ -4,9 +4,7 @@ class Image extends CI_Controller {
 	public function index()
 	{
 		$this->load->library('ProcessImage');
-		$folder = "images";
-		$images = $this->processimage->readIms($folder); 
-		$this->processimage->findDrawing($images, $folder);  
+		$this->processimage->test();  
 	}
 }
 ?>
