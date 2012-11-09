44068a31306aac97afe541c7d6c088c945feea40
Viktor Fröberg <viktorfroberg@gmail.com>
2012-11-09 15:08:11 +0100

    Uploaded images are now added to the database.
    Image-urls are added to the database with information about the date
    and group.

--- WebbApp/application/views/upload_form.php
+++ WebbApp/application/views/upload_form.php
@@ -1,9 +1,22 @@
 <h1>Ladda upp bilder här </h1>
-<?php //echo $error;?>
+<?php //echo $error;
+date_default_timezone_set('Europe/Stockholm');
+?>
 
 <?php echo form_open_multipart('index.php/site/create/upload');?>
 
 <input type="file" name="userfile[]" size="20" multiple=""/>
+
+
+<label for="group">
+	Gruppnamn
+</label>
+<input type="text" name="group" size="20"/>
+
+<label for="date">
+	Datum
+</label>
+<input type="text" name="date" value = <?php print "\"". date('Y-m-d') ."\" "; ?> size="20"/>
 
 <br /><br />
 
