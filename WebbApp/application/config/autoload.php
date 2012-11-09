6b5d3045236d5644b4640b3ab23d449fa37723cb
Viktor Fröberg <viktorfroberg@gmail.com>
2012-11-09 12:48:36 +0100

    Added a database.
    Create a user and a database on your local mysql-server, the webbapp
    will take care of the rest.
    To clean the database, write /ImmersiveKidz/WebbApp/?drop in the
    adressbar.
    
    hostname = 'localhost'
    username = 'ImmersiveKidz'
    password= '20n69O41992Nd54'
    database name = 'ImmersiveKidz'

--- WebbApp/application/config/autoload.php
+++ WebbApp/application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('database');
 
 
 /*
@@ -64,7 +64,7 @@ $autoload['libraries'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url');
+$autoload['helper'] = array('url', 'create_tables');
 
 
 /*
@@ -108,8 +108,8 @@ $autoload['language'] = array();
 |	$autoload['model'] = array('model1', 'model2');
 |
 */
-
-$autoload['model'] = array();
+// Install-model checks and create the images table
+$autoload['model'] = array('install_model');
 
 
 /* End of file autoload.php */
