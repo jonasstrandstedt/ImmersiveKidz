bdca05e9c7d10b53f9b7bcfe8b75ed801fdc0c28
Rickard Englund <rickard@r-englund.com>
2012-11-15 10:42:48 +0100

    Added test for strafe and vertical movments

--- DomApp/unit_test/Camera.test.h
+++ DomApp/unit_test/Camera.test.h
@@ -61,4 +61,14 @@ TEST (CameraTests, Movements) {
 	c->keyboardButton('S',0);
 	c->update(1);
 	EXPECT_EQ(glm::vec3(1,1.5,3),c->getPosition());
+
+	c->keyboardButton('Q',1);
+	c->update(1);
+	EXPECT_EQ(glm::vec3(1,2.5,3),c->getPosition());
+	
+	c->keyboardButton('Q',0);
+	c->keyboardButton('E',1);
+	c->update(1);
+	c->update(1);
+	EXPECT_EQ(glm::vec3(1,0.5,3),c->getPosition());
 }
