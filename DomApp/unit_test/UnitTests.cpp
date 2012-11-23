c838e1d89830951e106ae807248527891a5dbeab
Anders Nord <anders-nord@live.se>
2012-11-23 11:57:17 +0100

    Code review - Kanbanize id: 49

--- DomApp/unit_test/UnitTests.cpp
+++ DomApp/unit_test/UnitTests.cpp
@@ -2,13 +2,15 @@
 #include "gtest/gtest.h"
 #include "Camera.test.h"
 
-int runUnitTests(int argc, char **argv){
+int runUnitTests(int argc, char **argv)
+{
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
 
 #ifdef _RUN_TESTS_AT_STARTUP_
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
 	int res = runUnitTests(argc, argv);
 	std::cin.get();
 	return res;
