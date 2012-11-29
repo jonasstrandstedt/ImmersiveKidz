a0d79a3be177b3b7898d9ff15c8d679a7707c4ae
Anders Nord <anders-nord@live.se>
2012-11-29 14:43:31 +0100

    added billboardtest and HUDtest

---
+++ DomApp/unit_test/Billboard.test.h
@@ -0,0 +1,29 @@
+#include "../src/Billboard.h"
+
+#include "../src/ImmersiveKidz.h";
+
+/**
+* @file CameraTests.h
+* 
+* @author 	Rickard Englund, gefle.rickard@gmail.com
+* @date   	November 9, 2012
+* @version	0.0.0.1 
+*
+* @brief Tests the camera class
+*
+* @detials  A unit test case for the camera class
+*
+*/
+
+TEST (BillboardTest, Billtest)
+{
+	
+	Billboard *b = new Billboard(std::string("testString"), glm::vec3(0, 0, 0), glm::vec2(0 , 0));
+
+	
+
+
+
+
+
+}
---
+++ DomApp/unit_test/HUD.test.h
@@ -0,0 +1,29 @@
+#include "../src/HUD.h"
+
+#include "../src/ImmersiveKidz.h";
+
+/**
+* @file CameraTests.h
+* 
+* @author 	Rickard Englund, gefle.rickard@gmail.com
+* @date   	November 9, 2012
+* @version	0.0.0.1 
+*
+* @brief Tests the camera class
+*
+* @detials  A unit test case for the camera class
+*
+*/
+
+TEST (HUDtest, minimap)
+{
+	
+	HUD *h = new HUD();
+
+	
+
+
+
+
+
+}
