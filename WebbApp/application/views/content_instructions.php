67c350ab008556b98e8bddc70a697dcef1e14216
Svante Berg <svabe170@student.liu.se>
2012-12-04 11:50:45 +0100

    Encoding fix

--- WebbApp/application/views/content_instructions.php
+++ WebbApp/application/views/content_instructions.php
@@ -1,52 +1,53 @@
 <div id="contentInstructions">
 
-	<h1>Användarmanual</h1>
-	<h2>Installera på ny dator</h2>
+	<h1>Anv&auml;ndarmanual</h1>
+	<h2>Installera p&aring; ny dator</h2>
 	<p>
-		Det bildbehandlingsbibliotek som används är <a href="http://www.imagemagick.org/script/index.php"> ImageMagick </a>. Detta behöver vara 
-		installerat på den server där inläsningen av bilderna ska ske. Se “Instruktioner för utvecklare” för
-		information om hur du installerar ImageMagick på en ny dator. 
+		Det bildbehandlingsbibliotek som används &auml;r <a href="http://www.imagemagick.org/script/index.php"> ImageMagick </a>. Detta beh&ouml;ver vara 
+		installerat p&aring; den server d&auml;r inl&auml;sningen av bilderna ska ske. Se “Instruktioner f&ouml;r utvecklare” f&ouml;r
+		information om hur du installerar ImageMagick p&aring; en ny dator. 
 	</p>
 
-	<h2>Att tänka på innan</h2>
-	<p>Bilder som har klara färger och tydliga linjer ger ett bättre slutresultat. Informera om
-		möjligt detta för lärarna i förväg så att de är medvetna om att det påverkar resultatet. 
-		Försök att undvika att barnen skriver på framsidan av teckningen. Detta då programmet inte
-		särskiljer text från teckning. Om det ändå finns text på bilderna bör denna tas bort manuellt
-		innan bilderna läses in i programmet.  
+	<h2>Att t&auml;nka p&aring; innan</h2>
+	<p>Bilder som har klara f&auml;rger och tydliga linjer ger ett b&auml;ttre slutresultat. Informera om
+		m&ouml;jligt detta för l&auml;rarna i f&ouml;rv&auml;g så att de &auml;r medvetna om att det p&aring;verkar resultatet. 
+		F&ouml;rs&omlu;k att undvika att barnen skriver p&aring; framsidan av teckningen. Detta d&aring; programmet inte
+		s&auml;rskiljer text fr&aring;n teckning. Om det &auml;nd&aring; finns text p&aring; bilderna b&ouml;r denna tas bort manuellt
+		innan bilderna l&auml;ses in i programmet.  
 	</p>
 	<h2>Scanning</h2>
 	<p>
-		Scanna in önskade bilder till en mapp på datorn. Det gör inget om bilderna scannas in upp- och ner
-		då detta går att korrigera i programmet i efterhand.
+		Scanna in &ouml;nskade bilder till en mapp p&aring; datorn. Det g&ouml;r inget om bilderna scannas in upp- och ner
+		d&aring; detta g&aring;r att korrigera i programmet i efterhand.
 	</p>
 
 	<h2>Webbapplikationen</h2>
-	<p>1. Starta webbapplikationen från (här behövs webbadress)</p>
-	<p>2. Tryck på knappen "Välj filer" och navigera till mappen där de inscannade bilderna sparades.</p>
-	<p>3. Öppna mappen och markera alla bilder (Använd tangenterna Ctrl eller Shift för att markera flera bilder). Tryck "Öppna".</p>
-	<p>4. Fyll i gruppens namn och dagens datum i fälten. Tryck sedan på knappen "Ladda upp".</p>
-	<p>5. Bilderna visas nu frilagda. För att tydligare se om friläggningen lyckades kan bilderna visas i större format
-		genom att man klickar på bilden. Om friläggningen inte ser bra ut kan friläggningen anpassas genom att man drar
+	<p>1. Starta webbapplikationen fr&aring;n (h&auml;r beh&ouml;vs webbadress)</p>
+	<p>2. Tryck på knappen "V&auml;lj filer" och navigera till mappen d&auml;r de inscannade bilderna sparades.</p>
+	<p>3. &Ouml;ppna mappen och markera alla bilder (Anv&auml;nd tangenterna Ctrl eller Shift f&ouml;r att markera flera bilder). Tryck "&Ouml;ppna".</p>
+	<p>4. Fyll i gruppens namn och dagens datum i f&auml;lten. Tryck sedan p&aring; knappen "Ladda upp".</p>
+	<p>5. Bilderna visas nu frilagda. F&ouml;r att tydligare se om fril&auml;ggningen lyckades kan bilderna visas i st&ouml;rre format
+		genom att man klickar p&aring; bilden. Om fril&auml;ggningen inte ser bra ut kan fril&auml;ggningen anpassas genom att man drar
 		i reglaget som visas under varje bild.</p>
-	<p>6. Under varje bild finns fält för information. Fyll i informationen om vem som har ritat bilden, bildens namn
-		och vilken typ av objekt bilden föreställer (ex. fisk, fågel, landdjur). Det finns även ett fält där det går att
+	<p>6. Under varje bild finns f&auml;lt f&ouml;r information. Fyll i informationen om vem som har ritat bilden, bildens namn
+		och vilken typ av objekt bilden f&ouml;rest&auml;ller (ex. fisk, f&aring;gel, landdjur). Det finns &auml;ven ett f&auml;lt d&auml;r det g&aring;r att
 		att fylla i en anteckning eller historia om bilden.(valfritt)</p>
-	<p>7. (Valfritt) Det finns möjlighet att associera varje bild med ett ljud som spelas upp när man närmar sig bilden
-		i domapplikationen. Välj "Ladda upp ljud" för att associera en bild med en ljudfil som finns på datorn.</p>
-	<p>8. Tryck på "Gå vidare".</p>
-	<p>9. Tryck på "Ladda ner" för att spara filerna i en zip-fil på hårddisken. Zip-filen sparas i den mapp som är angiven
-	för filer nedladdade från internet.</p>
-	<p>10. Sätt i en USB-sticka i datorn och för över zip-filen till USB-stickan</p>
+	<p>7. (Valfritt) Det finns m&ouml;jlighet att associera varje bild med ett ljud som spelas upp n&auml;r man n&auml;rmar sig bilden
+		i domapplikationen. V&auml;lj "Ladda upp ljud" f&ouml;r att associera en bild med en ljudfil som finns p&aring; datorn.</p>
+	<p>8. Tryck på "G&aring; vidare".</p>
+	<p>9. Tryck på "Ladda ner" för att spara filerna i en zip-fil på h&aring;rddisken. Zip-filen sparas i den mapp som &auml;r angiven
+	f&ouml;r filer nedladdade fr&aring;n internet.</p>
+	<p>10. S&auml;tt i en USB-sticka i datorn och f&ouml;r &ouml;ver zip-filen till USB-stickan</p>
 
 	<h2>Domapplikationen</h2>
 	<p>Anvisning</p>
 	<p>Styrning - tangenter</p>
-	<p>Framåt - W</p>
-	<p>Bakåt - S</p>
-	<p>Uppåt - Q</p>
-	<p>Nedåt - E</p>
-	<p>Stereo AV/PÅ - X </p>
+	<p>Fram&aring;t - W</p>
+	<p>Bak&aring;t - S</p>
+	<p>Upp&aring;t - Q</p>
+	<p>Ned&aring;t - E</p>
+	<p>Stereo AV/P&Aring; - X </p>
 	<p>Avsluta - Esc</p>
-	
+
+
 </div>
