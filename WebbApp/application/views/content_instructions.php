192a84d3ec22964f3d23d95f98ccd0bba3bad42a
Svante Berg <svabe170@student.liu.se>
2012-12-04 12:06:46 +0100

    Encoding & Styles fix

--- WebbApp/application/views/content_instructions.php
+++ WebbApp/application/views/content_instructions.php
@@ -3,14 +3,14 @@
 	<h1>Anv&auml;ndarmanual</h1>
 	<h2>Installera p&aring; ny dator</h2>
 	<p>
-		Det bildbehandlingsbibliotek som används &auml;r <a href="http://www.imagemagick.org/script/index.php"> ImageMagick </a>. Detta beh&ouml;ver vara 
-		installerat p&aring; den server d&auml;r inl&auml;sningen av bilderna ska ske. Se “Instruktioner f&ouml;r utvecklare” f&ouml;r
+		Det bildbehandlingsbibliotek som anv&auml;nds &auml;r <a href="http://www.imagemagick.org/script/index.php"> ImageMagick </a>. Detta beh&ouml;ver vara 
+		installerat p&aring; den server d&auml;r inl&auml;sningen av bilderna ska ske. Se &quot;Instruktioner f&ouml;r utvecklare&quot; f&ouml;r
 		information om hur du installerar ImageMagick p&aring; en ny dator. 
 	</p>
 
 	<h2>Att t&auml;nka p&aring; innan</h2>
 	<p>Bilder som har klara f&auml;rger och tydliga linjer ger ett b&auml;ttre slutresultat. Informera om
-		m&ouml;jligt detta för l&auml;rarna i f&ouml;rv&auml;g så att de &auml;r medvetna om att det p&aring;verkar resultatet. 
+		m&ouml;jligt detta f&ouml;r l&auml;rarna i f&ouml;rv&auml;g så att de &auml;r medvetna om att det p&aring;verkar resultatet. 
 		F&ouml;rs&omlu;k att undvika att barnen skriver p&aring; framsidan av teckningen. Detta d&aring; programmet inte
 		s&auml;rskiljer text fr&aring;n teckning. Om det &auml;nd&aring; finns text p&aring; bilderna b&ouml;r denna tas bort manuellt
 		innan bilderna l&auml;ses in i programmet.  
@@ -23,7 +23,7 @@
 
 	<h2>Webbapplikationen</h2>
 	<p>1. Starta webbapplikationen fr&aring;n (h&auml;r beh&ouml;vs webbadress)</p>
-	<p>2. Tryck på knappen "V&auml;lj filer" och navigera till mappen d&auml;r de inscannade bilderna sparades.</p>
+	<p>2. Tryck p&auml; knappen "V&auml;lj filer" och navigera till mappen d&auml;r de inscannade bilderna sparades.</p>
 	<p>3. &Ouml;ppna mappen och markera alla bilder (Anv&auml;nd tangenterna Ctrl eller Shift f&ouml;r att markera flera bilder). Tryck "&Ouml;ppna".</p>
 	<p>4. Fyll i gruppens namn och dagens datum i f&auml;lten. Tryck sedan p&aring; knappen "Ladda upp".</p>
 	<p>5. Bilderna visas nu frilagda. F&ouml;r att tydligare se om fril&auml;ggningen lyckades kan bilderna visas i st&ouml;rre format
@@ -34,8 +34,8 @@
 		att fylla i en anteckning eller historia om bilden.(valfritt)</p>
 	<p>7. (Valfritt) Det finns m&ouml;jlighet att associera varje bild med ett ljud som spelas upp n&auml;r man n&auml;rmar sig bilden
 		i domapplikationen. V&auml;lj "Ladda upp ljud" f&ouml;r att associera en bild med en ljudfil som finns p&aring; datorn.</p>
-	<p>8. Tryck på "G&aring; vidare".</p>
-	<p>9. Tryck på "Ladda ner" för att spara filerna i en zip-fil på h&aring;rddisken. Zip-filen sparas i den mapp som &auml;r angiven
+	<p>8. Tryck p&aring; "G&aring; vidare".</p>
+	<p>9. Tryck p&aring; "Ladda ner" för att spara filerna i en zip-fil p&aring; h&aring;rddisken. Zip-filen sparas i den mapp som &auml;r angiven
 	f&ouml;r filer nedladdade fr&aring;n internet.</p>
 	<p>10. S&auml;tt i en USB-sticka i datorn och f&ouml;r &ouml;ver zip-filen till USB-stickan</p>
 
