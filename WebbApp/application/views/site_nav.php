dbb1a141cd7c17cd3840e7b79e2b6a9d0afb964c
emajl <emilindstrom@gmail.com>
2012-12-05 16:21:43 +0100

    styling and fixed logotype

--- WebbApp/application/views/site_nav.php
+++ WebbApp/application/views/site_nav.php
@@ -13,8 +13,8 @@
 <div id="container">	
 	<div id="nav">
 		<?php $img = img('styles/images/logotyp.png'); ?>
-		<div class="image"><?php echo anchor('".base_url()."', $img); ?>
-		</div>
+		<div class="image"><?php echo anchor('".base_url()."', $img); ?></div><div class="logo"><a href="<?=base_url();?>"><b>Norrköpings</b> Visualiseringscenter</a></div>
+		
 		<ul>
 			<li><?php echo anchor('index.php/site/upload', 'Skapa ny värld');?></li>
 			<li><?php echo anchor('index.php/site/add_information', 'Ändra befintlig värld');?></li>
